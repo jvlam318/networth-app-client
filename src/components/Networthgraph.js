a120cd81c7226ede30453ac94c2a48f764fa9458
joseph lam <jvlam318@gmail.com>
2020-05-19 22:14:22 +1000

    added login form

---
+++ src/components/Networthgraph.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class networthgraph extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Graph graph</h1>
+      </div>
+    )
+  }
+}
+
+export default networthgraph
+
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import Networthgraph from '../components/Networthgraph';
 
 class home extends Component {
+  state = {
+    networthHistory: null
+  }
+
+  componentDidMount() {
+    axios.get('/networthHistory')
+      .then(res => {
+        this.setState({
+          networthHistory: res.data
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    let networthHistory = this.state.networthHistory ? networthHistory : <p>Loading...</p>
     return (
-      <div>
-        <h1>home page</h1>
-      </div>
+      <Grid container spacing={8}>
+        <Grid item sm={8} xs={12}>
+          <Networthgraph networthHistory={networthHistory} />
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Summary</p>
+        </Grid>
+      </Grid>
     )
   }
 }
